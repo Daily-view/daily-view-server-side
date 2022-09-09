aa251dc20291096f619e4d32f6f7a094338199ca
nothingprogram <popawaw@naver.com>
2022-09-09 21:24:14 +0900

    implements to create member mutation

--- api/src/main/kotlin/com/daily/view/api/dto/CreateMemberInput.kt
+++ api/src/main/kotlin/com/daily/view/api/dto/CreateMemberInput.kt
@@ -1,9 +1,5 @@
 package com.daily.view.api.dto
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class CreateMemberInput(
     val email: String,
     val nickname: String,
--- api/src/main/kotlin/com/daily/view/api/fetcher/MemberQueryFetcher.kt
+++ api/src/main/kotlin/com/daily/view/api/fetcher/MemberQueryFetcher.kt
@@ -1,17 +1,19 @@
-//package com.daily.view.api.fetcher
-//
-//import com.daily.view.api.dto.CreateMemberInput
-//import com.daily.view.api.service.member.MemberService
-//import org.springframework.graphql.data.method.annotation.Argument
-//import org.springframework.graphql.data.method.annotation.QueryMapping
-//import org.springframework.stereotype.Controller
-//import reactor.core.publisher.Mono
-//
-//@Controller
-//class MemberQueryFetcher(
-//    private val memberService: MemberService
-//) {
-//
-//    @QueryMapping(value = "createMember")
-//    fun create(@Argument input: CreateMemberInput): Mono<Boolean> = memberService.create(input)
-//}
+package com.daily.view.api.fetcher
+
+import com.daily.view.api.dto.CreateMemberInput
+import com.daily.view.api.service.member.MemberService
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.stereotype.Controller
+import reactor.core.publisher.Mono
+
+@Controller
+class MemberMutationFetcher(
+    private val memberService: MemberService
+) {
+
+    @MutationMapping(value = "createMember")
+    fun signup(@Argument input: CreateMemberInput): Mono<Boolean> {
+        return memberService.create(input)
+    }
+}
