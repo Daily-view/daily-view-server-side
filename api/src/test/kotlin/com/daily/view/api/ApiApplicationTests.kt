64400cfd19779964034bdfb4bad3cbaea5b77881
nothingprogram <popawaw@naver.com>
2022-09-24 15:15:15 +0900

    test 깨지는 부분 수정

--- api/src/test/kotlin/com/daily/view/api/ApiApplicationTests.kt
+++ api/src/test/kotlin/com/daily/view/api/ApiApplicationTests.kt
@@ -1,3 +1,14 @@
 package com.daily.view.api
 
+import io.kotest.core.spec.style.BehaviorSpec
 
+class ApiApplicationTests : BehaviorSpec({
+
+    Given("a test") {
+        When("I run it") {
+            Then("it should pass") {
+                assert(true)
+            }
+        }
+    }
+})
